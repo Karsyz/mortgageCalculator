dbce9ce46110a9cfb27bec9338e5ddcc6f4dd9db
Karsyz <mattkars@gmail.com>
2024-01-11 19:12:57 -0500

    OOP refactor

---
+++ src/com/karsyz/Console.java
@@ -0,0 +1,22 @@
+package com.karsyz;
+
+import java.util.Scanner;
+
+public class Console {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
+    }
+    public static double readNumber(String prompt, double min, double max) {
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a value between " + (int) min + " and " + (int) max);
+        }
+        return value;
+    }
+}
